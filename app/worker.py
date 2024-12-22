4dc54409076a707bc0b4614b648ef7a63c3fa547
Muhamad Anjar <arvanria@gmail.com>
2024-12-23 01:36:18 +0700

    add dockerfile

--- app/worker.py
+++ app/worker.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 celery_app = Celery(
     'tasks',
-    broker='pyamqp://guest:guest@localhost//',  # RabbitMQ Broker
+    broker='amqp://guest@localhost//',  # RabbitMQ Broker
     backend='rpc://',  # Backend untuk tracking status
 )
